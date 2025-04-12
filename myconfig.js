6329c36de0047139693fc7617232500d8e1e77cf
aarifasultana <163408659+aarifasultana@users.noreply.github.com>
2025-04-12 11:24:11 -0500

    Add files via upload

---
+++ myconfig.js
@@ -0,0 +1,9 @@
+const myfirebaseConfig = {
+    apiKey: "AIzaSyBd5-WBH57ANdAGgRL8j-jHi0sb-iS8mpo",
+    authDomain: "job-portal-fdf7b.firebaseapp.com",
+    projectId: "job-portal-fdf7b",
+    storageBucket: "job-portal-fdf7b.firebasestorage.app",
+    messagingSenderId: "1071128368128",
+    appId: "1:1071128368128:web:2906fdb7558130b208b4e5",
+    measurementId: "G-57EW7LL2EG"
+  };
